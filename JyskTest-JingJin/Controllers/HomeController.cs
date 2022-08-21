4b89c006e4cf3d95d282409bbfa810a99d05e9ab
Jing Jin <jing.jin@2trace.com>
2022-08-21 13:39:26 +0200

    Add project files.

---
+++ JyskTest-JingJin/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using JyskTest_JingJin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RomanNumberConverter;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JyskTest_JingJin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly INumberConverter _romamNumberConverter;
+
+        public HomeController(ILogger<HomeController> logger, INumberConverter romamNumberConverter)
+        {
+            _logger = logger;
+            _romamNumberConverter = romamNumberConverter;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        /// <summary>
+        /// Input a roman number and conver it to integer
+        /// </summary>
+        /// <param name="number">A string which is roman number format</param>
+        /// <returns>A int to string and error message if any</returns>
+        public IActionResult RomanToInt(string number)
+        {
+            (int value, string error) = _romamNumberConverter.RomanToInt(number);
+            return new JsonResult(new ConverterResult { Text=value.ToString(), Error=error});
+        }
+        /// <summary>
+        /// Input a decimal number and conver it to Roman
+        /// </summary>
+        /// <param name="number">A string which is decimal number format</param>
+        /// <returns>Roman number and a error message if there is any</returns>
+        public IActionResult IntToRoman(string number)
+        {
+            Regex regex = new Regex(@"^\d+$");
+
+            if (!regex.IsMatch(number))
+            {
+                return new JsonResult(new ConverterResult { Text = String.Empty, Error = "Invalid number" });
+            }
+                (string text,string error) = _romamNumberConverter.IntToRoman(Int32.Parse(number));
+
+            return new JsonResult(new ConverterResult { Text = text, Error = error });
+        }
+    }
+    public class ConverterResult
+    {
+        public string Text { get; set; }
+        public string Error { get; set; }
+    }
+}
---
+++ RomanNumberConverter/RomanNumberConverter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RomanNumberConverter
+{
+    public interface INumberConverter
+    {
+        public (int, string) RomanToInt(string romanNumber);
+        public (string, string) IntToRoman(int number);
+    }
+    public class NumberConverter : INumberConverter
+    {
+        public (int, string) RomanToInt(string romanNumber)
+        {
+            if (string.IsNullOrEmpty(romanNumber))
+                return (-1, "Roman number cannot be empty. Please input an valid romertal");
+
+            romanNumber = romanNumber.ToUpper();
+            string formattingError = " is not correct roman number format. Please check your input";
+            string strRegex = @"[^IVXLCDM]|VV|LL|DD|[I]{4}|[X]{4}|[C]{4}|[M]{4}|IL|IC|ID|IM|VX|VL|VC|VD|VM|XD|XM|LC|LD|LM|DM|IIV|IIC|XXL|LLC|CCD|CCM";
+            Regex regex = new Regex(strRegex);
+            if (regex.IsMatch(romanNumber))
+            {
+                return (-1, romanNumber + formattingError);
+            }
+            int number = 0;
+            try
+            {
+                for (int counter = 0; counter < romanNumber.Length; counter++)
+                {
+                    var value = (int)Enum.Parse(typeof(RomanNumberEnum), romanNumber[counter].ToString());
+                    var nextValue = (counter < romanNumber.Length - 1) ? (int)Enum.Parse(typeof(RomanNumberEnum), romanNumber[counter + 1].ToString()) : 0;
+                    number = nextValue > value ? number - value : number + value;
+
+                }
+                return (number, string.Empty);
+            }
+            catch (System.Exception ex)
+            {
+                return (0, romanNumber + " error: " + ex.StackTrace);
+            }
+        }
+        /// <summary>
+        /// Convert a number to roman number, here vi assum the number is always positive. Negative number is not in the topic
+        /// </summary>
+        /// <param name="number">Positive digital number</param>
+        /// <returns>If input number is greater than 3000, return error. Else return roman number</returns>
+        public (string, string) IntToRoman(int number)
+        {
+            if (number > 3000)
+                return (string.Empty, "Number greater than 3000 is not supported");
+            //start from 1000
+            return (DivideIntIntoRomanCharactor(number, string.Empty, 3), String.Empty);
+        }
+        private string DivideIntIntoRomanCharactor(int number, string roman, int pow)
+        {
+            (string text, int remaining) = ConvertAIntToRoman((int)Math.Pow(10, pow), number, roman);
+            if (remaining > 0)
+            {
+                pow--;
+                text = DivideIntIntoRomanCharactor(remaining, text, pow);
+            }
+            return text;
+        }
+        private (string text, int rest) ConvertAIntToRoman(int pow, int number, string roman)
+        {
+            var index = number / pow;
+            if (index == 0)
+                return (roman, number);
+            var halfIndex = index > 5 ? index - 5 : index;
+            roman += halfIndex == 5 ? ((RomanNumberEnum)((index > 5 ? 10 : 5) * pow)).ToString() :
+                   halfIndex == 4 ? ((RomanNumberEnum)pow).ToString() + (RomanNumberEnum)((index > 5 ? 10 : 5) * pow) :
+                                     (index > 5 ? (RomanNumberEnum)(5 * pow) : string.Empty) + string.Empty.PadRight(halfIndex, char.Parse(((RomanNumberEnum)pow).ToString()));
+            return (roman, number - index * pow);
+        }
+    }
+    /// <summary>
+    /// Enum all roman charactors to int.
+    /// </summary>
+    public enum RomanNumberEnum
+    {
+        I = 1,
+        V = 5,
+        X = 10,
+        L = 50,
+        C = 100,
+        D = 500,
+        M = 1000
+    }
+
+}
+
